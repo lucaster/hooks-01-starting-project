01edb7fed714fa4717370e0a81157196e5c592bd
Luca <luca.cavagnoli@gmail.com>
2021-10-19 20:59:50 +0200

    format; refact

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,10 +1,9 @@
 import React from 'react';
-
 import './ErrorModal.css';
 
 const ErrorModal = React.memo(props => {
   return (
-    <React.Fragment>
+    <>
       <div className="backdrop" onClick={props.onClose} />
       <div className="error-modal">
         <h2>An Error Occurred!</h2>
@@ -15,7 +14,7 @@ const ErrorModal = React.memo(props => {
           </button>
         </div>
       </div>
-    </React.Fragment>
+    </>
   );
 });
 
