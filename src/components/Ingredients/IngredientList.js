1a5ad3cbbf0bd5f3773e7e0da6662a11c1aed3a4
Luca <luca.cavagnoli@gmail.com>
2021-10-19 21:01:00 +0200

    format

--- src/components/Ingredients/IngredientList.js
+++ src/components/Ingredients/IngredientList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './IngredientList.css';
 
 const IngredientList = props => {
@@ -8,7 +7,10 @@ const IngredientList = props => {
       <h2>Loaded Ingredients</h2>
       <ul>
         {props.ingredients.map(ig => (
-          <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
+          <li
+            key={ig.id}
+            onClick={props.onRemoveItem.bind(this, ig.id)}
+          >
             <span>{ig.title}</span>
             <span>{ig.amount}x</span>
           </li>
