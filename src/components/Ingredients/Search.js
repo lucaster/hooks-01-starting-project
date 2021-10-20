e15f1067dd29f56d775122f29cbefb5aa5897095
Luca <luca.cavagnoli@gmail.com>
2021-10-20 22:07:54 +0200

    input throttling via timeout (hacky)

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,40 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { FIREBASE_DB_URL } from '../../config/secrets';
 import Card from '../UI/Card';
 import './Search.css';
 
 const FIREBASE_DB_INGREDIENTS_URL = `${FIREBASE_DB_URL}/ingredients.json`;
+const FILTER_THROTTLE_MS = 500;
 
 const Search = React.memo(props => {
 
   const { onIngredientsLoaded } = props;
   const [filter, setFilter] = useState('');
+  const inputFilterRef = useRef();
 
   // rerun when filter changes:
   useEffect(() => {
     console.debug('Search', 'useEffect', 'fetch');
-    const query = filter ? `?orderBy="title"&equalTo="${filter}"` : '';
-    fetch(FIREBASE_DB_INGREDIENTS_URL + query)
-      .then(response => response.json())
-      .then(responseJson => {
-        const loadedIngredients = Object.keys(responseJson)
-          .map(key => {
-            const obj = responseJson[key];
-            return {
-              id: key,
-              title: obj.title,
-              amount: obj.amount
-            };
+    setTimeout(() => {
+      if (filter === inputFilterRef.current.value) {
+        // hasn't changed in the last 500ms
+        const query = filter ? `?orderBy="title"&equalTo="${filter}"` : '';
+          fetch(FIREBASE_DB_INGREDIENTS_URL + query)
+          .then(response => response.json())
+          .then(responseJson => {
+            const loadedIngredients = Object.keys(responseJson)
+              .map(key => {
+                const obj = responseJson[key];
+                return {
+                  id: key,
+                  title: obj.title,
+                  amount: obj.amount
+                };
+              });
+            return loadedIngredients;
+          })
+          .then(ingredients => {
+            // onIngredientsLoaded will change because the parent component
+            // is re-rendered and resets ingredientsLoadedHandler,
+            // thus causing loop:
+            onIngredientsLoaded(ingredients);
           });
-        return loadedIngredients;
-      })
-      .then(ingredients => {
-        // onIngredientsLoaded will change because the parent component
-        // is re-rendered and resets ingredientsLoadedHandler,
-        // thus causing loop:
-        onIngredientsLoaded(ingredients);
-      });
-  }, [filter, onIngredientsLoaded]);
+      }
+    }, FILTER_THROTTLE_MS);
+  }, [filter, onIngredientsLoaded, inputFilterRef]);
 
   const onChangeFilter = event => {
     setFilter(event.target.value)
@@ -47,6 +54,7 @@ const Search = React.memo(props => {
           <label>Filter by Title</label>
           <input
             type="text"
+            ref={inputFilterRef}
             value={filter}
             onChange={onChangeFilter}
           />
